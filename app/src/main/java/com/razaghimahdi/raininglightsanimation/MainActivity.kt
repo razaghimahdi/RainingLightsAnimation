8787d5822ddfaf016411b0bb0e9cec3fcc5e4993
razaghimahdi <razaghimahdi78@gmail.com>
2023-03-25 15:59:46 +0330

    - Updated MainActivity.kt

--- app/src/main/java/com/razaghimahdi/raininglightsanimation/MainActivity.kt
+++ app/src/main/java/com/razaghimahdi/raininglightsanimation/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.razaghimahdi.raininglightsanimation.ui.theme.RainingLightsAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RainingLightsAnimationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    RainingLightsAnimation()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RainingLightsAnimationTheme {
-        Greeting("Android")
+
+
+
+
+
+
+    override fun onStart() {
+        super.onStart()
+        hideUISystem()
+    }
+
+    private fun hideUISystem() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
