2296b1e4821964782d6daccd0a7129af80c9cb46
razaghimahdi <razaghimahdi78@gmail.com>
2023-03-25 16:12:03 +0330

    - Updated Theme.kt

--- app/src/main/java/com/razaghimahdi/raininglightsanimation/ui/theme/Theme.kt
+++ app/src/main/java/com/razaghimahdi/raininglightsanimation/ui/theme/Theme.kt
@@ -9,13 +9,15 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = grey_1000_b,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = grey_1000_b,
 
     /* Other default colors to override
     background = Color.White,
